eca64711cd7d06e5ec7c9da81a90ee615acff55a
ParisAlan <alanparis2003@gmail.com>
2025-11-20 17:20:55 +0100

    Travail sur SimpleRenderer.php

--- src/Template/SimpleRenderer.php
+++ src/Template/SimpleRenderer.php
@@ -2,18 +2,24 @@
 
 use Framework312\Router\Exception as RouterException;
 use Framework312\Template\Renderer;
-use Symfony\Component\HttpFoundation\Request; // Rajout de Request
-use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment; // On rajoute ce qu'il faut pour le bon fonctionnement de Twig
+use Twig\Loader\FilesystemLoader; // On rajoute ce qu'il faut pour le bon fonctionnement de Twig
 
 class TwigRenderer implements Renderer {
 
-    public function __construct(Renderer $engine) {
+    private FilesystemLoader $loader; // On met en place le systemLoader comme $engine a été mis en place dans SimpleRouter
+    private Environment $twig; // On met en place l'environnement
 
+    public function __construct(string $templateDir) { // dans l'exemple, il reçoit /templates/
+
+        // Partie Basics de la documentation : https://twig.symfony.com/doc/3.x/api.html
+        $this->loader = new FilesystemLoader($templateDir);
+        $this->twig = new Environment($this->loader);
     }
 
     public function render(mixed $data, string $template): string
     {
-        // TODO
+        return $this->twig->render($template, $data); // https://twig.symfony.com/doc/3.x/api.html
     }
 
     public function register(string $tag)
