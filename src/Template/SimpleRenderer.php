53395d245e27df46d5e33d67e070b4fb2537917d
ParisAlan <alanparis2003@gmail.com>
2025-11-21 15:54:35 +0100

    Travail sur SimpleRenderer.php

--- src/Template/SimpleRenderer.php
+++ src/Template/SimpleRenderer.php
@@ -7,11 +7,13 @@ use Twig\Loader\FilesystemLoader; // On rajoute ce qu'il faut pour le bon foncti
 
 class TwigRenderer implements Renderer {
 
+    private string $templateDir;
     private FilesystemLoader $loader; // On met en place le systemLoader comme $engine a été mis en place dans SimpleRouter
     private Environment $twig; // On met en place l'environnement
 
     public function __construct(string $templateDir) { // dans l'exemple, il reçoit /templates/
 
+        $this->templateDir = $templateDir;
         // Partie Basics de la documentation : https://twig.symfony.com/doc/3.x/api.html
         $this->loader = new FilesystemLoader($templateDir);
         $this->twig = new Environment($this->loader);
@@ -20,10 +22,19 @@ class TwigRenderer implements Renderer {
     public function render(mixed $data, string $template): string
     {
         return $this->twig->render($template, $data); // https://twig.symfony.com/doc/3.x/api.html
+        // On utilise pas twig->load() car render simple le faire aussi mais en prenant en compte que y'a $data
     }
 
-    public function register(string $tag)
-    {
-        // TODO
+    public function register(string $tag): void {
+
+        $lastChar = substr($this->templateDir, -1); // On utilise substr pour aller chercher le dernier caractère
+
+        // Pour éviter des bugs ou des doubles slash, si le dernier caractère n'est pas un "/", on lui rajoute
+        if ( $lastChar !== '/') {
+            $this->templateDir .= '/';
+        }
+
+        $path = $this->templateDir . $tag;
+        $this->loader->addPath($path, $tag); // On spécifique que le "namespace" sera @"tag"
     }
 }
